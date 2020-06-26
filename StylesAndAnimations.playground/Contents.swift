98cb93464fdbad9e3bac4f7358266f2975556c8d
jon.lasley@gmail.com <jon.lasley@gmail.com>
2020-06-26 11:02:33 -0400

    Drawn heart, list presentation, dimsiss taps

--- StylesAndAnimations.playground/Contents.swift
+++ StylesAndAnimations.playground/Contents.swift
@@ -3,22 +3,35 @@
 import UIKit
 import PlaygroundSupport
 
-class MyViewController : UIViewController {
-	
-    override func loadView() {
-        let view = UIView()
-        view.backgroundColor = .white
-		
-		let button = ThreeBarButton(x: 50, y: 50, height: 20)
-		button.backgroundColor = .white
-		view.addSubview(button)
-		self.view = view
-    }
-	
-	let dispatch = DispatchGroup()
-	
-}
+let shadow: NSShadow = {
+	let s = NSShadow()
+	s.shadowColor = UIColor.black
+	s.shadowBlurRadius = 4.0
+	return s
+}()
+let attributes: [NSAttributedString.Key : Any] = [
+	.font: UIFont(name: "MarkerFelt-Thin", size: 32) as Any,
+//	.shadow: shadow,
+	.foregroundColor: #colorLiteral(red: 1, green: 0.8822258415, blue: 0.4357810227, alpha: 1),
+	.strokeColor: #colorLiteral(red: 0.2767290609, green: 0.2767290609, blue: 0.2767290609, alpha: 1),
+	.strokeWidth: -2.0,
+	.kern: -3.0,
+]
 
+let title = NSMutableAttributedString(string: "Your LoveList", attributes: attributes)
+title.setAttributes([NSAttributedString.Key.kern: 3.0], range: NSRange(location: 4, length: 1))
+
+
+let label: UILabel = {
+	let l = UILabel()
+	l.frame = CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: 300, height: 48))
+	l.attributedText = title
+	l.textAlignment = .center
+	l.baselineAdjustment = .alignCenters
+	l.backgroundColor = #colorLiteral(red: 0.6916132569, green: 0.5701339841, blue: 0.929064393, alpha: 1)
+	return l
+}()
+
+PlaygroundPage.current.liveView = label
 
 // Present the view controller in the Live View window
-PlaygroundPage.current.liveView = MyViewController()
