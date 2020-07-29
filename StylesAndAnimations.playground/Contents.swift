ee3ecc03bc6e9d8fc495af049a2b359e431d2f6d
jon.lasley@gmail.com <jon.lasley@gmail.com>
2020-07-29 17:43:21 -0400

    Bunch of stuff added

--- StylesAndAnimations.playground/Contents.swift
+++ StylesAndAnimations.playground/Contents.swift
@@ -3,29 +3,31 @@
 import UIKit
 import PlaygroundSupport
 
-let shadow: NSShadow = {
-	let s = NSShadow()
-	s.shadowColor = UIColor.black
-	s.shadowBlurRadius = 4.0
-	return s
-}()
-let attributes: [NSAttributedString.Key : Any] = [
-	.font: UIFont(name: "MarkerFelt-Thin", size: 32) as Any,
+//let shadow: NSShadow = {
+//	let s = NSShadow()
+//	s.shadowColor = UIColor.black
+//	s.shadowBlurRadius = 4.0
+//	return s
+//}()
+//let attributes: [NSAttributedString.Key : Any] = [
 //	.shadow: shadow,
-	.foregroundColor: #colorLiteral(red: 1, green: 0.8822258415, blue: 0.4357810227, alpha: 1),
-	.strokeColor: #colorLiteral(red: 0.2767290609, green: 0.2767290609, blue: 0.2767290609, alpha: 1),
-	.strokeWidth: -2.0,
-	.kern: -3.0,
-]
+//	.foregroundColor: #colorLiteral(red: 1, green: 0.8822258415, blue: 0.4357810227, alpha: 1),
+//	.font: UIFont(name: "Noteworthy", size: 60)!
+//]
+//
+//let title = NSMutableAttributedString(string: "Hi, Jon", attributes: attributes)
 
-let title = NSMutableAttributedString(string: "Your LoveList", attributes: attributes)
-title.setAttributes([NSAttributedString.Key.kern: 3.0], range: NSRange(location: 4, length: 1))
+func createLoveLetters() -> String {
+	let letters = "ABCDEFGHIJKILMNOPQRSTUVWXYZ"
+	var l = String((0..<5).map({ _ in letters.randomElement()! }))
+	return l
+}
 
 
 let label: UILabel = {
 	let l = UILabel()
-	l.frame = CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: 300, height: 48))
-	l.attributedText = title
+	l.frame = CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: 300, height: 200))
+	l.text = createLoveLetters()
 	l.textAlignment = .center
 	l.baselineAdjustment = .alignCenters
 	l.backgroundColor = #colorLiteral(red: 0.6916132569, green: 0.5701339841, blue: 0.929064393, alpha: 1)
